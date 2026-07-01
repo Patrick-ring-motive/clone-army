8531c79ffc287b30baaed0dbc6f9dcf1901be818
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-06-30 19:36:19 -0500

    Create core.js

---
+++ core.js
@@ -0,0 +1,31 @@
+function cloneMap(map){
+  try{
+    return structuredClone(map);
+  }catch{
+    return new Map(map);
+  }
+}
+
+function cloneSet(set){
+  try{
+    return structuredClone(set);
+  }catch{
+    return new Set(set);
+  }
+}
+
+function cloneArray(arr){
+  try{
+    return structuredClone(arr);
+  }catch{
+    return [...arr];
+  }
+}
+
+function cloneObject(obj){
+  try{
+    return structuredClone(obj);
+  }catch{
+    return Object.create(obj);
+  }
+}
