f07c909a7c5f33584f65ded6030ceb5fc5e95b63
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-07-01 12:19:14 -0500

    Create web.js

---
+++ web.js
@@ -0,0 +1,27 @@
+function cloneHeaders(headers){
+  try{
+    return structuredClone(headers);
+  }catch{
+    const clone = new Headers();
+    for(const [key,value] of headers){
+      try{
+        if(!/^set-cookie$/i.test(key){
+          clone.append(key,value);
+        }
+      }catch{
+        continue;
+      }
+    }
+    if(headers.has('set-cookie')){
+      if(Headers.prototype.getSetCookie){
+        const cookies = headers.getSetCookie();
+        for(const cookie of cookies){
+          clone.append('set-cookie',cookie);
+        }
+      }else{
+        clone.append('set-cookie',headers.get('set-cookie');
+      }
+    }
+    return clone;
+  }
+}
