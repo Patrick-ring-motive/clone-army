614d88629de976e000d9137f4ae9927bf30ec268
MissingLink <60206943+Patrick-ring-motive@users.noreply.github.com>
2026-07-02 03:05:03 -0500

    style: js-beautify formatting (web.js)

--- web.js
+++ web.js
@@ -1,25 +1,25 @@
-function cloneHeaders(headers){
-  try{
+function cloneHeaders(headers) {
+  try {
     return structuredClone(headers);
-  }catch{
+  } catch {
     const clone = new Headers();
-    for(const [key,value] of headers){
-      try{
-        if(!/^set-cookie$/i.test(key)){
-          clone.append(key,value);
+    for (const [key, value] of headers) {
+      try {
+        if (!/^set-cookie$/i.test(key)) {
+          clone.append(key, value);
         }
-      }catch{
+      } catch {
         continue;
       }
     }
-    if(headers.has('set-cookie')){
-      if(Headers.prototype.getSetCookie){
+    if (headers.has('set-cookie')) {
+      if (Headers.prototype.getSetCookie) {
         const cookies = headers.getSetCookie();
-        for(const cookie of cookies){
-          clone.append('set-cookie',cookie);
+        for (const cookie of cookies) {
+          clone.append('set-cookie', cookie);
         }
-      }else{
-        clone.append('set-cookie',headers.get('set-cookie'));
+      } else {
+        clone.append('set-cookie', headers.get('set-cookie'));
       }
     }
     return clone;
